69c6a5cc9a1282308214a787889b70e6f743f3ed
Derek Chaplin <wolfborg@users.noreply.github.com>
2022-04-06 16:26:59 -0400

    initial files & python3 conversion

---
+++ specs/stylesheet/schema.js
@@ -0,0 +1,14 @@
+// var term_list = document.getElementsByTagName("a");
+
+// for (var i=0; i<term_list.length; i++) {
+//   var element = term_list[i];
+//   element.addEventListener("mouseover", function( event ) {
+//     // highlight the mouseover target
+//     //event.target.innerHTML = "test";
+//     var info_box = document.getElementById("info_box");
+//     info_box.innerHTML = event.target.getAttribute("description");
+//     alert("test");
+//   }, false);
+// }
+
+// console.log(term_list);
