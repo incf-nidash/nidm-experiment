c13857f309defc4c17e583bf1927c76d2e4de8f4
Derek Chaplin <wolfborg@users.noreply.github.com>
2022-05-20 12:37:27 -0400

    datatype page generation setup

--- scripts/nidm_schema.py
+++ scripts/nidm_schema.py
@@ -15,19 +15,16 @@ logging.basicConfig(filename='debug.log', level=logging.DEBUG, filemode='w')
 logger = logging.getLogger(__name__)
 
 class OwlNidmHtml:
-    def __init__(self, term_infos, import_files, spec_name, schema_file=None):
-        self.name = spec_name
-        self.component = self.name.lower().replace("-", "_")
+    def __init__(self, term_infos, import_files, type="class"):
+        self.schema_file = "schema_"+type+".html"
+        self.type = type
+
+        self.create_schema_file()
         self.section_open = 0
         self.already_defined_classes = list()
 
         for terms in term_infos:
-            prefix, term_prefix = terms['prefix']
             owl_file = terms['owl_file']
-
-            self.prefix = prefix
-            self.term_prefix = term_prefix
-
             self.owl = OwlReader(owl_file, import_files)
             self.owl.graph.bind('owl', 'http://www.w3.org/2002/07/owl#')
             self.owl.graph.bind('dct', 'http://purl.org/dc/terms/')
@@ -40,17 +37,25 @@ class OwlNidmHtml:
             self.owl.graph.bind('qibo', 'http://www.owl-ontologies.com/Ontology1298855822.owl')
             self.owl.graph.bind('sio', 'http://semanticscience.org/resource/')
             
-            #self.classes = self.split_process(owl_file)
-
-            self.schema_file = schema_file
-            self.schema_text = "<div class=\"container-fluid\"><section id=\"viewer\"><h2>NIDM Class Browser</h2><div class=\"row\"><div class=\"col-5\"><div id='schema' class='list-group list-group-root well'>"
+            self.schema_text = ""
             self.schema_done = []
 
-            #self.create_specification(prefix)
-            self.create_schema_spec()
-            self.add_schema()
+            if type == "class":
+                self.create_schema_class_spec()
+            elif type == "datatype":
+                self.create_schema_type_spec(OWL['DatatypeProperty'])
+            elif type == "annotation":
+                self.create_schema_type_spec(OWL['AnnotationProperty'])
+            elif type == "object":
+                self.create_schema_type_spec(OWL['ObjectProperty'])
+            elif type == "individual":
+                self.create_schema_type_spec(OWL['NamedIndividual'])
 
-    def create_schema_spec(self):
+            self.add_schema()
+        
+        self.schema_footer()
+
+    def create_schema_class_spec(self):
         prov_types = [PROV['Activity'], PROV['Entity'], PROV['Agent'], None]
 
         for prov in prov_types:
@@ -63,6 +68,7 @@ class OwlNidmHtml:
                     prov_def = "<i>Definition not found</i>"
                 self.schema_text += "<a class='list-group-item' data-bs-toggle='collapse' role='button' href=\"#"+prov_name+"\" description=\""+prov_def+"\" aria-expanded='true'>"+prov_link+"</a>"
                 self.schema_text += "<div class='list-group multi-collapse level-1 show' id=\""+prov_name+"\">"
+            
             children = self.owl.get_direct_children(prov)
             children = self.owl.sorted_by_labels(children)
 
@@ -72,15 +78,50 @@ class OwlNidmHtml:
                     if len(parents) > 0:
                         continue
 
-                self.get_hierarchy(child, path=prov_name)
+                self.get_hierarchy_subclass(child, path=prov_name)
+            self.schema_text += "</div>"
+    
+    def create_schema_type_spec(self, rdf_type):
+        terms = self.owl.all_of_rdf_type(rdf_type, but_type=OWL['Class'])
+
+        for term in terms:
+            term_link = self.owl.get_label(term)
+            term_name = self.owl.get_name(term)
+            term_def = self.format_definition(self.owl.get_definition(term))
+            if not term_def:
+                term_def = "<i>Definition not found</i>"
+
+            children = self.get_prop_children(term)
+            children = self.owl.sorted_by_labels(children)
+
+            if len(children) <= 0:
+                self.schema_text += "<a description=\""+term_def+"\" role=\"button\" class=\"list-group-item\" tag=\""+term_name+"\">"+term_link+"</a>"
+                continue
+
+            self.schema_text += "<a class='list-group-item' data-bs-toggle='collapse' role='button' href=\"#"+term_name+"\" description=\""+term_def+"\" aria-expanded='true'>"+term_link+"</a>"
+            self.schema_text += "<div class='list-group multi-collapse level-1 show' id=\""+term_name+"\">"
+            for child in children:
+                self.get_hierarchy_subprop(child, path=term_name)
             self.schema_text += "</div>"
 
-        # self.add_type_section(OWL['DatatypeProperty'])
-        # self.add_type_section(OWL['AnnotationProperty'])
-        # self.add_type_section(OWL['ObjectProperty'])
-        # self.add_type_section(OWL['NamedIndividual'])
-    
-    def get_hierarchy(self, uri, level=1, path=""):
+        self.schema_text += "</div>"
+
+    def get_prop_children(self, term):
+        children = set()
+
+        for child_name in self.owl.graph.subjects(RDFS['subPropertyOf'], term):
+            if not self.owl.is_deprecated(child_name):
+                children.add(child_name)
+
+        return children
+
+    def get_prop_parents(self, term):
+        parents = set()
+        for parent_name in self.owl.graph.objects(term, RDFS['subPropertyOf']):
+            parents.add(parent_name)
+        return parents
+
+    def get_hierarchy_subclass(self, uri, level=1, path=""):
         class_label = self.owl.get_label(uri)
         self.schema_done.append(class_label)
         
@@ -115,9 +156,49 @@ class OwlNidmHtml:
         self.schema_text += "<div class=\"list-group multi-collapse "+hier_level+" collapse\" id=\""+class_name+"\">"
 
         for child in children:
-            self.get_hierarchy(child, level+1, path)
+            self.get_hierarchy_subclass(child, level+1, path)
 
         self.schema_text += "</div>"
+
+    def get_hierarchy_subprop(self, uri, level=1, path=""):
+        prop_label = self.owl.get_label(uri)
+        self.schema_done.append(prop_label)
+        
+        prop_name = self.owl.get_name(uri)
+        definition = self.format_definition(self.owl.get_definition(uri))
+        
+        if not definition:
+            definition = "<i>Definition not found</i>"
+        description = definition
+
+        text_break = "</br>------------------------</br>"
+        description += text_break
+
+        # term_info = self.generate_info(uri)
+        # if term_info:
+        #     description = definition+text_break+term_info
+        
+        description = description.replace('"', '&quot;')
+        description = description.replace("'", '&apos;')
+
+        path += "/"+prop_name
+        description += path
+
+        children = self.get_prop_children(uri)
+        children = self.owl.sorted_by_labels(children)
+        if len(children) <= 0:
+            self.schema_text += "<a description=\""+description+"\" role=\"button\" class=\"list-group-item\" tag=\""+prop_name+"\">"+prop_label+"</a>"
+            return None
+        
+        hier_level = "level-"+str(level+1)
+        self.schema_text += "<a href=\"#"+prop_name+"\" description=\""+description+"\" role=\"button\" data-bs-toggle=\"collapse\" class=\"list-group-item\" tag=\""+prop_name+"\" aria-expanded='false'>"+prop_label+"</a>"
+        self.schema_text += "<div class=\"list-group multi-collapse "+hier_level+" collapse\" id=\""+prop_name+"\">"
+
+        for child in children:
+            self.get_hierarchy_subprop(child, level+1, path)
+
+        self.schema_text += "</div>"
+
 
     def format_definition(self, definition):
         try:
@@ -149,12 +230,10 @@ class OwlNidmHtml:
 
         nidm_class = self.owl.get_nidm_parent(class_uri)
         if nidm_class:
-            #print(self.term_prefix+"-n: "+nidm_class)
             text += " a "+self.owl.get_label(nidm_class)
         else:
             prov_class = self.owl.get_prov_class(class_uri)
             if prov_class:
-                #print(self.term_prefix+": "+prov_class)
                 text += " a "+self.owl.get_label(prov_class)
             else:
                 #look in NIDM file
@@ -209,86 +288,83 @@ class OwlNidmHtml:
         schema_open = codecs.open(schema_file, 'a', "utf-8")
         schema_open.write(self.schema_text+"\n</div>")
         schema_open.close()
-  
 
-    # def split_process(self, owl_file):
-    #     f = open(owl_file, "r")
-    #     lines = f.readlines()
-    #     f.close()
-        
-    #     classes = []
-    #     for x in lines:
-    #         x = x.strip()
-    #         if x != "" and x[0] != "#" and x[0] != "@" and x[0] != "[":
-    #             if "owl:Class" not in x:
-    #                 continue
-    #             #print(x)
-    #             x = shlex.split(x)
-    #             subject = x[0]
-    #             if subject not in classes:
-    #                 classes.append(subject)
+    def create_schema_file(self):
+        if self.schema_file != None:
+            schema_file = os.path.join(DOC_FOLDER, self.schema_file)
 
-    #     return classes
+        if self.type == "class":
+            title = "NIDM Class Browser"
+        if self.type == "datatype":
+            title = "NIDM Datatype Property Browser"
+        if self.type == "annotation":
+            title = "NIDM Annotation Property Browser"
+        if self.type == "object":
+            title = "NIDM Object Property Browser"
+        if self.type == "individual":
+            title = "NIDM Named Individual Browser"
 
-def create_schema_file(schema_file="schema_class.html"):
-    if schema_file == "schema_class.html":
-        schema_file = os.path.join(DOC_FOLDER, "schema_class.html")
-    schema_open = codecs.open(schema_file, 'w', "utf-8")
-    schema_open.write("""
-    <!DOCTYPE html>
-    <html lang="en-us">
-    <head>
-        <meta charset="UTF-8">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="stylesheet/schema.css" media="screen">
-        <script type="text/javascript" src="stylesheet/schema.js"></script>
+        schema_open = codecs.open(schema_file, 'w', "utf-8")
+        schema_open.write("""
+        <!DOCTYPE html>
+        <html lang="en-us">
+        <head>
+            <meta charset="UTF-8">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+            <link rel="stylesheet" type="text/css" href="stylesheet/schema.css" media="screen">
+            <script type="text/javascript" src="stylesheet/schema.js"></script>
 
-        <link rel="stylesheet" type="text/css" href="stylesheet/normalize.css" media="screen">
-        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" type="text/css" href="stylesheet/stylesheet.css" media="screen">
-        <link rel="stylesheet" type="text/css" href="stylesheet/github-light.css" media="screen">
-    </head>
-    <body>
-        <section class="page-header">
-            <h1 class="project-name">Schema Browser: Class</h1>
-            <a href="schema_class.html" class="btn">Class</a>
-            <a href="schema_datatype.html" class="btn">Datatype Property</a>
-            <a href="schema_annotation.html" class="btn">Annotation Property</a>
-            <a href="schema_object.html" class="btn">Object Property</a>
-            <a href="schema_individual.html" class="btn">Named Individual</a>
-            <a href="index.html" class="btn">Term Resolution Page</a>
-        </section>
-    """)
-    #<div id="info_box">test</div>
-    #""")
-    schema_open.close()
+            <link rel="stylesheet" type="text/css" href="stylesheet/normalize.css" media="screen">
+            <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
+            <link rel="stylesheet" type="text/css" href="stylesheet/stylesheet.css" media="screen">
+            <link rel="stylesheet" type="text/css" href="stylesheet/github-light.css" media="screen">
+        </head>
+        <body>
+            <section class="page-header">
+                <h1 class="project-name">Schema Browser: Class</h1>
+                <a href="schema_class.html" class="btn">Class</a>
+                <a href="schema_datatype.html" class="btn">Datatype Property</a>
+                <a href="schema_annotation.html" class="btn">Annotation Property</a>
+                <a href="schema_object.html" class="btn">Object Property</a>
+                <a href="schema_individual.html" class="btn">Named Individual</a>
+                <a href="index.html" class="btn">Term Resolution Page</a>
+            </section>
+            <div class=\"container-fluid\"><section id=\"viewer\">
+                <h2>"""+title+"""</h2>
+                <div class=\"row\">
+                <div class=\"col-5\"><div id='schema' class='list-group list-group-root well'>
+        """)
+        #<div id="info_box">test</div>
+        #""")
+        schema_open.close()
 
-def schema_footer(schema_file="schema_class.html"):
-    if schema_file == "schema_class.html":
-        schema_file = os.path.join(DOC_FOLDER, "schema_class.html")
-    schema_open = codecs.open(schema_file, 'a', "utf-8")
-    schema_open.write("""
-            <div class="col-7">
-                <div id="infoBoard" class="border border-primary rounded">
-                    <h4 id="title">Term</h4>
-                    <p id="description">Definition</p>
+    def schema_footer(self):
+        if self.schema_file != None:
+            schema_file = os.path.join(DOC_FOLDER, self.schema_file)
+
+        schema_open = codecs.open(schema_file, 'a', "utf-8")
+        schema_open.write("""
+                <div class="col-7">
+                    <div id="infoBoard" class="border border-primary rounded">
+                        <h4 id="title">Term</h4>
+                        <p id="description">Definition</p>
+                    </div>
                 </div>
             </div>
+            </section>
+            <section class="main-content">
+            <footer class="site-footer">
+                <span class="site-footer-owner"><a href="https://github.com/incf-nidash/nidm-experiment">NIDM-Experiment</a> is maintained by <a href="https://github.com/wolfborg">wolfborg</a>.</span>
+                <span class="site-footer-credits">This page was generated by <a href="https://pages.github.com">GitHub Pages</a> using the <a href="https://github.com/jasonlong/cayman-theme">Cayman theme</a> by <a href="https://twitter.com/jasonlong">Jason Long</a>.</span>
+            </footer>   
+            </section>
         </div>
-        </section>
-        <section class="main-content">
-        <footer class="site-footer">
-            <span class="site-footer-owner"><a href="https://github.com/incf-nidash/nidm-experiment">NIDM-Experiment</a> is maintained by <a href="https://github.com/wolfborg">wolfborg</a>.</span>
-            <span class="site-footer-credits">This page was generated by <a href="https://pages.github.com">GitHub Pages</a> using the <a href="https://github.com/jasonlong/cayman-theme">Cayman theme</a> by <a href="https://twitter.com/jasonlong">Jason Long</a>.</span>
-        </footer>   
-        </section>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    </body>
-    </html>
-    """)
-    schema_open.close()
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        </body>
+        </html>
+        """)
+        schema_open.close()
 
 
 RELPATH = os.path.dirname(os.path.abspath(__file__))
@@ -306,42 +382,15 @@ def main():
     # check the file exists
     assert os.path.exists(owl_file)
 
-    schema_file="schema_class.html"
-    create_schema_file(schema_file=schema_file)
-
     term_infos = [
-        {'prefix': [str(NIDM), 'nidm'], 'owl_file': os.path.join(TERMS_FOLDER, 'nidm-experiment.owl')},
-        #{'prefix': [str(BIDS), 'bids'], 'owl_file': os.path.join(IMPORTS_FOLDER, 'bids_import.ttl')},
-        #{'prefix': [str(DICOM), 'dicom'], 'owl_file': os.path.join(IMPORTS_FOLDER, 'dicom_import.ttl')},
-        #{'prefix': [str(SIO), 'sio'], 'owl_file': os.path.join(IMPORTS_FOLDER, 'sio_import.ttl')},
-        #{'prefix': [str(OBO), 'obo'], 'owl_file': os.path.join(IMPORTS_FOLDER, 'obo_import.ttl')},
-        #{'prefix': [str(ONLI), 'onli'], 'owl_file': os.path.join(IMPORTS_FOLDER, 'ontoneurolog_instruments_import.ttl')},
+        {'prefix': [str(NIDM), 'nidm'], 'owl_file': owl_file},
     ]
-
-    OwlNidmHtml(term_infos, import_files, "NIDM-Experiment", schema_file)
-
-    #owlspec = OwlNidmHtml(owl_file, import_files, "NIDM-Experiment", prefix=str(NIDM), term_prefix="nidm", schema_file=schema_file)
     
-    #component_name = "nidm-experiment"
-    #owlspec.write_specification(component=component_name, version=nidm_version)
-
-    
-    #owl_file = os.path.join(IMPORTS_FOLDER, 'bids_import.ttl')
-    #owl_process(owl_file, None, "BIDS", prefix=str(BIDS), term_prefix="bids", schema_file=schema_file)
-    
-    # owl_file = os.path.join(IMPORTS_FOLDER, 'dicom_import.ttl')
-    # owl_process(owl_file, None, "DICOM", prefix=str(DICOM), term_prefix="dicom")
-
-    # owl_file = os.path.join(IMPORTS_FOLDER, 'sio_import.ttl')
-    # owl_process(owl_file, None, "SIO", prefix=str(SIO), term_prefix="sio")
-    
-    # owl_file = os.path.join(IMPORTS_FOLDER, 'obo_import.ttl')
-    # owl_process(owl_file, None, "OBO", prefix=str(OBO), term_prefix="obo")
-
-    #owl_file = os.path.join(IMPORTS_FOLDER, 'ontoneurolog_instruments_import.ttl')
-    #owl_process(owl_file, None, "ONLI", prefix=str(ONLI), term_prefix="onli", schema_file=schema_file)
-
-    schema_footer(schema_file="schema_class.html")
+    OwlNidmHtml(term_infos, import_files, "class")
+    OwlNidmHtml(term_infos, import_files, "datatype")
+    # OwlNidmHtml(term_infos, import_files, "annotation")
+    # OwlNidmHtml(term_infos, import_files, "object")
+    # OwlNidmHtml(term_infos, import_files, "individual")
 
 if __name__ == "__main__":
     main()
